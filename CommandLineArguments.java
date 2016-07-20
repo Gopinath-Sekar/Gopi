435360567a6dbb54b433224d2a371110c2968874
Gopinath-Sekar <gopinath.seee@ksrce.ac.in>
2016-07-20 12:26:56 +0530

    Create CommandLineArguments.java

---
+++ CommandLineArguments.java
@@ -0,0 +1,8 @@
+public class CommandLineArguments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(args[0] + "  " + "technologies" + " " + args[1]);
+	}
+
+}
